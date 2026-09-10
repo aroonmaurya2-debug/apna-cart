b2168012c15d87c4747ebfc1ce7356a48b776213
aroonmaurya2-debug <aroonmaurya2@gmail.com>
2026-09-10 10:14:02 -0700

    Load compact mobile home styles

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import './home-fix.css'
+import './home-mobile.css'
 import App from './AppFixed.tsx'
 import MarketplaceFeatures from './MarketplaceFeatures.tsx'
 
