38449a604888f716682d69c8a079c1ddc838d5b8
aroonmaurya2-debug <aroonmaurya2@gmail.com>
2026-09-10 09:50:27 -0700

    Add Home filter option constants

---
+++ src/home-filter-options.ts
@@ -0,0 +1,8 @@
+export const HOME_FILTER_GENDERS = ['All', 'Women', 'Men', 'Kids', 'Unisex'] as const
+export const HOME_FILTER_SORTS = [
+  ['relevance', '↕ Sort'],
+  ['price-low', 'Price: Low to High'],
+  ['price-high', 'Price: High to Low'],
+  ['rating', 'Top Rated'],
+  ['discount', 'Best Discount'],
+] as const
