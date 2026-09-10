b7b02cc8793dfbcbd8b06984df502b524a1cb1f3
aroonmaurya2-debug <aroonmaurya2@gmail.com>
2026-09-10 09:51:46 -0700

    Remove unused temporary filter options file

--- src/home-filter-options.ts
+++
@@ -1,8 +0,0 @@
-export const HOME_FILTER_GENDERS = ['All', 'Women', 'Men', 'Kids', 'Unisex'] as const
-export const HOME_FILTER_SORTS = [
-  ['relevance', '↕ Sort'],
-  ['price-low', 'Price: Low to High'],
-  ['price-high', 'Price: High to Low'],
-  ['rating', 'Top Rated'],
-  ['discount', 'Best Discount'],
-] as const
