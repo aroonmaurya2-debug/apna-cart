de203da5f0b9e1578aaf34aff9e0192e2a56aa7a
aroonmaurya2-debug <aroonmaurya2@gmail.com>
2026-09-09 19:26:55 -0700

    Fix PWA cache so latest Apna Cart version updates

--- public/sw.js
+++ public/sw.js
@@ -1,5 +1,5 @@
-const CACHE_NAME = 'apna-cart-v5'
-const APP_SHELL = ['/', '/manifest.webmanifest', '/app-icon.svg?version=5']
+const CACHE_NAME = 'apna-cart-v6'
+const APP_SHELL = ['/', '/manifest.webmanifest', '/app-icon.svg?version=6']
 
 self.addEventListener('install', (event) => {
   event.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(APP_SHELL)))
@@ -7,14 +7,19 @@ self.addEventListener('install', (event) => {
 })
 
 self.addEventListener('activate', (event) => {
-  event.waitUntil(self.clients.claim())
+  event.waitUntil(
+    caches.keys().then((keys) => Promise.all(keys.filter((key) => key !== CACHE_NAME).map((key) => caches.delete(key))))
+      .then(() => self.clients.claim()),
+  )
 })
 
 self.addEventListener('fetch', (event) => {
   if (event.request.method !== 'GET') return
-  event.respondWith(caches.match(event.request).then((cached) => cached || fetch(event.request).then((response) => {
-    const copy = response.clone()
-    caches.open(CACHE_NAME).then((cache) => cache.put(event.request, copy))
-    return response
-  }).catch(() => caches.match('/'))))
+  event.respondWith(
+    caches.match(event.request).then((cached) => cached || fetch(event.request).then((response) => {
+      const copy = response.clone()
+      caches.open(CACHE_NAME).then((cache) => cache.put(event.request, copy))
+      return response
+    }).catch(() => caches.match('/'))),
+  )
 })
