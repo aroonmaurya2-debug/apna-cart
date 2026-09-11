ff7e69b727833f6cd756ddfd13b40356a2798a9d
aroonmaurya2-debug <aroonmaurya2@gmail.com>
2026-09-11 11:18:14 -0700

    Clear stale service-worker cache on app startup

--- public/api-fix.js
+++ public/api-fix.js
@@ -8,4 +8,17 @@
     else if (input instanceof URL) input = new URL(input.toString().replace(OLD_API_HOST, LIVE_API_HOST));
     return originalFetch(input, init);
   };
+
+  // Clear old Apna Cart service-worker control/cache so stale GitHub Pages
+  // bundles cannot keep the app on a blank screen after a deployment.
+  if ('serviceWorker' in navigator) {
+    navigator.serviceWorker.getRegistrations()
+      .then((registrations) => Promise.all(registrations.map((registration) => registration.unregister())))
+      .catch(() => undefined);
+  }
+  if ('caches' in window) {
+    caches.keys()
+      .then((keys) => Promise.all(keys.filter((key) => key.startsWith('apna-cart-')).map((key) => caches.delete(key))))
+      .catch(() => undefined);
+  }
 })();
