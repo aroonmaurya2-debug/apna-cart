a74915a5a4f4e4892933d68ab34f37a440fd0364
aroonmaurya2-debug <aroonmaurya2@gmail.com>
2026-09-16 09:27:13 -0700

    Add owner app React entry

---
+++ src/admin-entry.tsx
@@ -0,0 +1,5 @@
+import { createRoot } from 'react-dom/client'
+import AdminPanel from './AdminPanel'
+import './admin-panel.css'
+
+createRoot(document.getElementById('root')!).render(<AdminPanel />)
